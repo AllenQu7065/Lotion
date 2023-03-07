571c7c7b6a3ebc69017426fae4327c94d5366e82
AllenQu7065 <allen.qu@ucalgary.ca>
2023-03-06 21:35:38 -0700

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React, { useState } from "react";
+
 function App() {
-  return <h1>Lotion</h1>;
+  const [notes] =  useState([])
+  
+  return (
+    <>
+      <h1>Lotion Project</h1>
+      <NotesList />
+      <input type="text" />
+      <button>Add Notes</button>
+      <button>Clear Notes</button>
+      <div>0 left notes</div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/NotesList.js
@@ -0,0 +1,8 @@
+function NotesList() {
+
+    return (
+      <div>
+        <p>You clicked 0 times</p>
+      </div>
+    );
+  }
